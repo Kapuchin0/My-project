cbd5306d648b13d018a2adb5452f4dd9671e9f61
Kapuchin0 <rebus.7878@gmail.com>
2022-08-22 05:59:39 +0300

    сделано

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,27 @@
-const humburger = document.querySelector('.header__login__button');
-const mobileMenu = document.querySelector('.popup');
+const login = document.querySelector('.header__login__button');
+const pop = document.querySelector('.popup');
 const overlay = document.querySelector('.popup__overlay');
 const cros = document.querySelector('.popup__btn-close')
 
-humburger.addEventListener('click', openMenu);
+login.addEventListener('click', openMenu);
 overlay.addEventListener('click', openMenu);
 cros.addEventListener('click', openMenu)
 
 function openMenu() {
-    mobileMenu.classList.toggle('popup--active');
+    pop.classList.toggle('popup--active');
     overlay.classList.toggle("popup__overlay--active");
+}
+
+const humburger = document.querySelector('.header_humburger');
+const mobileMenu = document.querySelector('.header__mobile');
+// const overlay = document.querySelector('.popup__overlay');
+// const mcros = document.querySelector('.popup__btn-close')
+
+humburger.addEventListener('click', openMenum);
+// overlay.addEventListener('click', openMenum);
+// mcros.addEventListener('click', openMenu)
+
+function openMenum() {
+    mobileMenu.classList.toggle('header__mobile--active');
+    // overlay.classList.toggle("popup__overlay--active");
 }
--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-$('.flowing-scroll').on('click', function() {
-    var el = $(this);
-    var dest = el.attr('href'); 
-    if (dest !== undefined && dest !== '') { 
-      $('html').animate({
-          scrollTop: $(dest).offset().top 
-        }, 100 // 
-      );
-    }
-    return false;
+// $('.flowing-scroll').on('click', function() {
+//     var el = $(this);
+//     var dest = el.attr('href'); 
+//     if (dest !== undefined && dest !== '') { 
+//       $('html').animate({scrollTop: $(dest).offset().top }, 1500);
+//     }
+//     return false;
+//   });
+
+
+ 
+  $(document).ready(function(){
+    $('.flowing-scroll').on("click", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+  
+      //забираем идентификатор бока с атрибута href
+      var id  = $(this).attr('href'),
+  
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+      
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({scrollTop: top}, 1500, linear);
+    });
   });
-
-
